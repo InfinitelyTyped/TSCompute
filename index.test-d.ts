a7219190798134c50b32c8325986790722f7b623
Cassie Choi <nrocinu.lahwran@gmail.com>
2021-04-25 11:05:57 -0400

    Add 4 & 8 bit adder tests

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,6 +1,14 @@
-import { Bit, OneBitAdder } from ".";
+import {
+  Bit,
+  EightBitAdder,
+  EightBitNumber,
+  FourBitAdder,
+  FourBitNumber,
+  OneBitAdder,
+} from ".";
 import { expectType } from "tsd";
 
+/** ============ One Bit Adder ============ */
 declare function oneBitAdd<A extends Bit, B extends Bit, C extends Bit = 0>(
   a: A,
   b: B,
@@ -15,3 +23,35 @@ expectType<[0, 1]>(oneBitAdd(1, 0, 0));
 expectType<[1, 0]>(oneBitAdd(1, 0, 1));
 expectType<[1, 0]>(oneBitAdd(1, 1, 0));
 expectType<[1, 1]>(oneBitAdd(1, 1, 1));
+
+/** ============ Four Bit Adder ============ */
+declare function fourBitAdd<
+  A extends FourBitNumber,
+  B extends FourBitNumber,
+  C extends Bit = 0
+>(a: A, b: B, c: C): FourBitAdder<A, B, C>;
+
+expectType<[0, 0, 0, 0, 0]>(fourBitAdd([0, 0, 0, 0], [0, 0, 0, 0], 0));
+expectType<[1, 0, 0, 0, 0]>(fourBitAdd([1, 0, 0, 1], [0, 1, 1, 1], 0));
+expectType<[1, 0, 0, 0, 0]>(fourBitAdd([1, 1, 1, 1], [0, 0, 0, 0], 1));
+expectType<[1, 1, 1, 1, 1]>(fourBitAdd([1, 1, 1, 1], [1, 1, 1, 1], 1));
+
+/** ============ Eight Bit Adder ============ */
+declare function eightBitAdd<
+  A extends EightBitNumber,
+  B extends EightBitNumber,
+  C extends Bit = 0
+>(a: A, b: B, c: C): EightBitAdder<A, B, C>;
+
+expectType<[0, 0, 0, 0, 0, 0, 0, 0, 0]>(
+  eightBitAdd([0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], 0)
+);
+expectType<[1, 0, 0, 0, 0, 0, 0, 0, 0]>(
+  eightBitAdd([1, 0, 0, 0, 0, 0, 0, 1], [0, 1, 1, 1, 1, 1, 1, 1], 0)
+);
+expectType<[1, 0, 0, 0, 0, 0, 0, 0, 0]>(
+  eightBitAdd([1, 1, 1, 1, 1, 1, 1, 1], [0, 0, 0, 0, 0, 0, 0, 0], 1)
+);
+expectType<[1, 1, 1, 1, 1, 1, 1, 1, 1]>(
+  eightBitAdd([1, 1, 1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1, 1, 1], 1)
+);
